30c35473d49581f7a03be1bf8ec8ad3282624232
VZero911 <vzero911@gmail.com>
2024-12-05 04:13:29 +0100

    Str

--- printf/ft_printf_str.c
+++ printf/ft_printf_str.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 20:13:05 by marvin            #+#    #+#             */
-/*   Updated: 2024/12/05 03:07:34 by marvin           ###   ########.fr       */
+/*   Updated: 2024/12/05 04:00:57 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,43 +49,43 @@ char	*ft_ulltoa_base(t_ull n, const char *base)
 
 static char *prepare_string(t_struct *data)
 {
-    char *str;
-    char *print_str;
+	char *str;
+	char *print_str;
 
-    str = data->var.s;
-    if (!str)
-    {
-        if (data->precision == -1 || data->precision >= 6)
-            str = "(null)";
-        else
-            str = "";
-    }
-    if (data->precision != -1 && data->precision < (int)ft_strlen(str))
-        print_str = ft_substr(str, 0, data->precision);
-    else
-        print_str = ft_strdup(str);
-    return (print_str);
+	str = data->var.s;
+	if (!str)
+	{
+		if (data->precision == -1 || data->precision >= 6)
+			str = "(null)";
+		else
+			str = "";
+	}
+	if (data->precision != -1 && data->precision < (int)ft_strlen(str))
+		print_str = ft_substr(str, 0, data->precision);
+	else
+		print_str = ft_strdup(str);
+	return (print_str);
 }
 
 void ft_printf_string(t_struct *data)
 {
-    char *print_str;
-    int strlen;
-    int padlen;
-    char padding;
+	char *print_str;
+	int strlen;
+	int padlen;
+	char padding;
 
-    print_str = prepare_string(data);
-    if (!print_str)
-        return ;
-    strlen = ft_strlen(print_str);
-    if (data->width > strlen)
-        padlen = data->width - strlen;
-    else
-        padlen = 0;
-    padding = padding_char(data);
-    print_and_pad(print_str, padlen, padding, data);
-    if (!(data->width > strlen))
-        data->width = strlen;
-    ft_data_len(data, data->width);
-    free(print_str);
+	print_str = prepare_string(data);
+	if (!print_str)
+		return ;
+	strlen = ft_strlen(print_str);
+	if (data->width > strlen)
+		padlen = data->width - strlen;
+	else
+		padlen = 0;
+	padding = padding_char(data);
+	print_and_pad(print_str, padlen, padding, data);
+	if (!(data->width > strlen))
+		data->width = strlen;
+	ft_data_len(data, data->width);
+	free(print_str);
 }
