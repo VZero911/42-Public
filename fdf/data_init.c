0bd26ec74363ee4a3bfc0169d9efeac546eb1035
Judicael Dumay <jdumay@c1r7p2.42lehavre.fr>
2024-12-13 21:25:43 +0100

    Win_Height xD

--- fdf/data_init.c
+++ fdf/data_init.c
@@ -102,7 +102,7 @@ int	data_init(t_fdf *data)
 	data->mlx_ptr = mlx_init();
 	if (!data->mlx_ptr)
 		return (MLX_ERROR);
-	data->win_ptr = mlx_new_window(data->mlx_ptr, WIN_WIDTH, WIN_WIDTH, "FdF");
+	data->win_ptr = mlx_new_window(data->mlx_ptr, WIN_WIDTH, WIN_HEIGHT, "FdF");
 	if (!data->win_ptr)
 		return (MLX_ERROR);
 	data->image = mlx_new_image(data->mlx_ptr, WIN_WIDTH, WIN_HEIGHT);
